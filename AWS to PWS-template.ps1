8fbdf932a0e1cc7b0dfe450841dee9238ba21bd2
butchbutchart <42536650+butchbutchart@users.noreply.github.com>
2023-06-27 09:23:28 +0100

    Update AWS to PWS-template.ps1

--- AWS to PWS-template.ps1
+++ AWS to PWS-template.ps1
@@ -1,17 +1,33 @@
-﻿# Import the AWS Tools for PowerShell module make sure to install if not already installed
+# Import the AWS Tools for PowerShell module
 Import-Module -Name AWSPowerShell.NetCore
 
 # Set your AWS credentials (replace with your own values)
 $awsAccessKey = "FILL-ME-IN"
 $awsSecretKey = "FILL-ME-IN"
 $awsRegion = "Global"  # Replace with your desired AWS region
-$awsGroup = "FILL-ME-IN"  # Replace with your desired AWS group
+$awsGroup = "FILL-ME-IN"
 
 # Set the BeyondTrust API details (replace with your own values)
 $baseUrl = "FILL-ME-IN"
 $apiKey = "FILL-ME-IN"
-$runAsUser = "apiuser"
-$systemName = "FILL-ME-IN" # managed system in Password Safe that the accounts will be added to
+$runAsUser = "FILL-ME-IN" # API User
+$systemName = "FILL-ME-IN" # Password Safe managed system where IAM users will be added
+
+# Create AWS credentials using the provided access key and secret key
+$awsCredentials = New-Object -TypeName Amazon.Runtime.BasicAWSCredentials -ArgumentList $awsAccessKey, $awsSecretKey
+
+# Set AWS credentials and region in the AWS Tools for PowerShell module
+Set-AWSCredentials -AccessKey $awsAccessKey -SecretKey $awsSecretKey
+Set-DefaultAWSRegion -Region $awsRegion
+
+# Request the AWS credential report
+Request-IAMCredentialReport
+
+# Wait for the report to be generated
+Start-Sleep -Seconds 10
+
+# Retrieve the AWS credential report
+$report = Get-IAMCredentialReport -AsTextArray
 
 #Used to bypass any cert errors.
 #region Trust All Certificates
@@ -32,21 +48,7 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
 ";
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy;
 
-# Create AWS credentials using the provided access key and secret key
-$awsCredentials = New-Object -TypeName Amazon.Runtime.BasicAWSCredentials -ArgumentList $awsAccessKey, $awsSecretKey
 
-# Set AWS credentials and region in the AWS Tools for PowerShell module
-Set-AWSCredentials -AccessKey $awsAccessKey -SecretKey $awsSecretKey
-Set-DefaultAWSRegion -Region $awsRegion
-
-# Request the AWS credential report
-Request-IAMCredentialReport
-
-# Wait for the report to be generated
-Start-Sleep -Seconds 10
-
-# Retrieve the AWS credential report
-$report = Get-IAMCredentialReport -AsTextArray
 
 # Build the Authorization header
 $headers = @{ Authorization="PS-Auth key=${apiKey}; runas=${runAsUser}" }
@@ -58,15 +60,16 @@ $signInResult = Invoke-RestMethod -Uri "${baseUrl}Auth/SignAppIn" -Method POST -
 $managedSystems = Invoke-RestMethod -Uri "${baseUrl}ManagedSystems?Name=${systemName}" -Method GET -Headers $headers -ContentType "application/json" -WebSession $session
 $managedSystemId = $managedSystems.ManagedSystemId
 
-# Filter the report array for users in the "Test-Managed" group with password_enabled = "true"
+# Filter the report array for users in the $awsGroup group with password_enabled = "true"
 $filteredReportArray = $reportArray | Where-Object { $_.user -in (Get-IAMGroup -GroupName $awsGroup | Select-Object -ExpandProperty Users).UserName -and $_."password_enabled" -eq "true" }
+
 
 # Create assets in BeyondTrust Password Safe
 foreach ($item in $filteredReportArray) {
     $body = @{
         AccountName = $item.user
         Password = "blank"
-        Description = $item.UserId
+        Description = "Imported via script"
     } | ConvertTo-Json
 
     Invoke-RestMethod -Uri "${baseUrl}ManagedSystems/${managedSystemId}/ManagedAccounts" -Method POST -Body $body -Headers $headers -ContentType "application/json" -WebSession $session
